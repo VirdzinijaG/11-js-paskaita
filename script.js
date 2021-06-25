f4b2c3315ddd9b0c4fae380cce9c7662d545a9eb
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-06-25 10:24:49 +0300

    dvimatis masyvas, konstruktoriaus funkcija

--- script.js
+++ script.js
@@ -111,3 +111,105 @@ for (let x in zmogus1.automobiliai[i].modelius) {
 
 }
 }
+
+
+// masyvai
+
+let skaiciai = [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let i in skaiciai) {
+    console.log(skaiciai[i]); // isvedamas kiekvienas skaicius atskirai
+}
+
+// dvimatis masyvas masyvas masyve
+
+let skaiciai1 = [
+    [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10],
+    [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10],
+    [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10],
+    [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10],
+    [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10],
+    [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10],
+    [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10],
+    [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10],
+    [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10],
+    [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10],
+]
+console.log(skaiciai1);
+
+for (let i in skaiciai1) {
+    console.log(skaiciai1[i]); // isvedami kiekvienas elementas, isvedami masyvo masyvai, eilutes
+}
+
+console.log(skaiciai1[6]); // isvedamas masyvo kitas masyvas kuris yra 6
+console.log(skaiciai1[6][8]); // isvedamas masyvo 6 eilutes(kito masyvo) 8 elementas
+
+// isvesti visus skaicius kurie yra dvimaciame masyve
+for (let i in skaiciai1[6]) {
+    console.log(skaiciai1[6][i]); // 7 eilutes visi elementai
+}
+
+for (let i in skaiciai1) { // i praeina pro visas eilutes
+    for (let j in skaiciai1[i]) { // j praeina pro visus stulpelius
+        console.log(skaiciai1[i][j]); // isvedami dvimacio masyvo visi skaiciai atskirai
+    }
+}
+
+let skaiciai1Ilgis = skaiciai1.length
+console.log(skaiciai1Ilgis); // masyvo elementai - 10, 10 eiluciu kiti masyvai
+
+let eilutesIlgis = skaiciai1.length;
+for (let i = 0; i < eilutesIlgis; i++) {
+    let stulpelioIlgis = skaiciai1[i].length // kievienos eilutes ilgi. Masyvo kievkieno masyvo ilgis
+    console.log(stulpelioIlgis);
+}
+
+for (let i = 0; i < eilutesIlgis; i++) {
+    let stulpelioIlgis = skaiciai1[i].length;
+    for (let j = 0; j < stulpelioIlgis; j++) {
+        console.log(skaiciai1[i][j]); // isvedami dvimacio masyvo visi skaiciai atskirai
+    }
+}
+
+
+////////
+
+const skaiciuotuvas = {
+    sk1: 1,
+    sk2: 2,
+
+    paleidimas: function(sk1, sk2) {
+        this.sk1 = sk1;
+        this.sk2 = sk2;
+    }
+}
+skaiciuotuvas.paleidimas(4,6)
+
+// objekto sablonas (vienoda struktura, vienodi metodai, tie patys propierties), 100
+// 4 zmogaus objektai
+// sukurti objekta pagal tam tikra sablona
+
+
+// Apibrezti konstruktoriaus funkcija - pagal kuria susikonstruoja objektas
+// Tai nera objektas, tai yra objekto sablonas
+function Zmogus(vardas, pavarde, asmensKodas) { // Konstruktoriaus funkcija prasideda is didziosios raides
+    this.vardas = vardas;    // sukonstruota busimo objekto sablonas
+    this.pavarde = pavarde;
+    this.asmensKodas = asmensKodas;
+}
+
+
+
+
+
+const z1 = new Zmogus("Tomas", "Tomaitis", "3589455"); // new Zmogus, naudojamas konstruktorius, pagal kuri sukuria objekta
+console.log(z1);
+
+const z2 = new Zmogus("Petras", "Petraitis", "5412655")
+console.log(z2);
+
+const z3 = new Zmogus("Jonas", "Jonaitis", "1359943")
+console.log(z3);
+
+const z4 = new Zmogus("Antanas", "Antanaitis", "54582231")
+console.log(z4);
